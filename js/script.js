1a865dde1d2763cad899aa5b721fc811519367dc
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-23 17:56:24 +0100

    provati i 3 riempitori per ogni difficoltà

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 // creo la funzione che mi popola il box di quadrati
+// funzione che crea 100 quadrati
+
+// function box100(){
+// const createBox = document.querySelector('.box-container');
+// for( i = 1; i<= 100; i++){
+//     // myValue = i;
+//     const newBox = document.createElement('div');
+//     newBox.classList.add('box-100');
+//     const textInside = document.createElement('span');
+//     createBox.append(newBox);
+    
+// }
+// }
+// function box81(){
+// const createBox = document.querySelector('.box-container');
+// for( i = 1; i<= 81; i++){
+//     // myValue = i;
+//     const newBox = document.createElement('div');
+//     newBox.classList.add('box-81');
+//     const textInside = document.createElement('span');
+//     createBox.append(newBox);
+    
+// }
+// }
+// function box49(){
+const createBox = document.querySelector('.box-container');
+for( i = 1; i<= 49; i++){
+    // myValue = i;
+    const newBox = document.createElement('div');
+    newBox.classList.add('box-49');
+    const textInside = document.createElement('span');
+    createBox.append(newBox);
+
+}
+// }
